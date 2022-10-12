eda9f476419510367fe4961cf287eb636f02af1c
Devany15 <76234922+Devany15@users.noreply.github.com>
2022-10-12 18:00:25 -0500

    Registros, estadísticas, fuentes y colores

--- app/src/main/java/com/example/ldmnannyapp/Alimentos.kt
+++ app/src/main/java/com/example/ldmnannyapp/Alimentos.kt
@@ -1,6 +1,7 @@
 package com.example.ldmnannyapp
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -8,16 +9,18 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposeCompilerApi
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
-import com.example.ldmnannyapp.ui.theme.LDMNannyAppTheme
-import com.example.ldmnannyapp.ui.theme.amarillito
+import com.example.ldmnannyapp.ui.theme.*
 
 class Alimentos : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,10 +43,12 @@ class Alimentos : ComponentActivity() {
 fun vistaAlimentos() {
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         topBar()
-        Spacer(Modifier.padding(5.dp))
+        Spacer(Modifier.height(5.dp))
         title()
-        Spacer(Modifier.padding(5.dp))
+        Spacer(Modifier.height(15.dp))
         reg()
+        Spacer(Modifier.height(30.dp))
+        est()
     }
 
 }
@@ -72,43 +77,165 @@ fun title(modifier: Modifier = Modifier)
                 modifier = Modifier
                     .paddingFromBaseline(7.dp, 0.dp)
                     .size(35.dp))
-            Text(text = "ALIMENTOS",
-                fontSize = 17.sp,
-                fontWeight = FontWeight.Medium,
-                modifier = modifier.paddingFromBaseline(30.dp),
-
+            Text("ALIMENTOS",
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = ConcertOne,
+                color = letra,
+                modifier = modifier.paddingFromBaseline(32.dp,0.dp),
             )
         }
-
     }
 }
 
 @Composable
 fun reg() {
-    Scaffold(Modifier.size(300.dp, 200.dp), backgroundColor = amarillito) {
+    Scaffold(Modifier.size(300.dp,160.dp), backgroundColor = amarillito) {
         Column() {
-            Text(text = "Últimos registros")
-            Spacer(Modifier.size(10.dp))
-            Row(horizontalArrangement = Arrangement.Center) {
-                Text(text = "1", modifier = Modifier.size(50.dp, 50.dp))
-                Text(text = "2", modifier = Modifier.size(50.dp, 50.dp))
-                Text(text = "3", modifier = Modifier.size(50.dp, 50.dp))
+            Text(text = "Últimos registros",
+                textAlign = TextAlign.Center,
+                fontSize = 20.sp,
+                fontFamily = ConcertOne,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .paddingFromBaseline(25.dp, 0.dp))
+            Spacer(Modifier.height(10.dp))
+            Row() {
+                Text("1",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .width(100.dp)
+                        .height(40.dp)
+                        .paddingFromBaseline(27.dp),
+                    fontFamily = SpaceMono,
+                    fontWeight = FontWeight.Normal
+                )
+                Text("2",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .width(100.dp)
+                        .height(40.dp)
+                        .paddingFromBaseline(27.dp),
+                    fontFamily = SpaceMono,
+                    fontWeight = FontWeight.Normal
+                )
+                Text("3",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .width(100.dp)
+                        .height(40.dp)
+                        .paddingFromBaseline(27.dp),
+                    fontFamily = SpaceMono,
+                    fontWeight = FontWeight.Normal
+                )
             }
 
             Row() {
-                Text(text = "4", modifier = Modifier.size(50.dp, 50.dp))
-                Text(text = "5", modifier = Modifier.size(50.dp, 50.dp))
-                Text(text = "6", modifier = Modifier.size(50.dp, 50.dp))
+                Text("4",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .width(100.dp)
+                        .height(40.dp)
+                        .paddingFromBaseline(27.dp),
+                    fontFamily = SpaceMono,
+                    fontWeight = FontWeight.Normal
+                )
+                Text("5",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .width(100.dp)
+                        .height(40.dp)
+                        .paddingFromBaseline(27.dp),
+                    fontFamily = SpaceMono,
+                    fontWeight = FontWeight.Normal
+                )
+                Text("6",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .width(100.dp)
+                        .height(40.dp)
+                        .paddingFromBaseline(27.dp),
+                    fontFamily = SpaceMono,
+                    fontWeight = FontWeight.Normal
+                )
             }
             Row() {
-                Text(text = "7", modifier = Modifier.size(50.dp, 50.dp))
-                Text(text = "8", modifier = Modifier.size(50.dp, 50.dp))
-                Text(text = "9", modifier = Modifier.size(50.dp, 50.dp))
+                Text("7",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .width(100.dp)
+                        .height(40.dp)
+                        .paddingFromBaseline(27.dp),
+                    fontFamily = SpaceMono,
+                    fontWeight = FontWeight.Normal
+                )
+                Text("8",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .width(100.dp)
+                        .height(40.dp)
+                        .paddingFromBaseline(27.dp),
+                    fontFamily = SpaceMono,
+                    fontWeight = FontWeight.Normal
+                )
+                Text("9",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .width(100.dp)
+                        .height(40.dp)
+                        .paddingFromBaseline(27.dp),
+                    fontFamily = SpaceMono,
+                    fontWeight = FontWeight.Normal
+                )
             }
         }
     }
 }
 
+@Composable
+fun est(){
+    Scaffold(Modifier.size(300.dp, 200.dp), backgroundColor = amarillito) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+            Spacer(Modifier.height(40.dp))
+            Text("Tu bebé toma fórmula cada X horas",
+                textAlign = TextAlign.Center,
+                fontFamily = SpaceMono,
+                fontWeight = FontWeight.Normal,
+                fontSize = 13.sp,
+            )
+            Spacer(Modifier.height(8.dp))
+            Text("Tu bebé come papilla cada X horas",
+                fontFamily = SpaceMono,
+                fontWeight = FontWeight.Normal,
+                fontSize = 13.sp,
+                textAlign = TextAlign.Center
+            )
+            Spacer(Modifier.height(8.dp))
+            Text("Tu bebé toma leche materna cada X horas",
+                fontFamily = SpaceMono,
+                fontWeight = FontWeight.Normal,
+                fontSize = 13.sp,
+                textAlign = TextAlign.Center
+            )
+            Spacer(Modifier.height(8.dp))
+            Text("Tu bebé toma leche X veces al día",
+                fontFamily = SpaceMono,
+                fontWeight = FontWeight.Normal,
+                fontSize = 13.sp,
+                textAlign = TextAlign.Center
+            )
+            Spacer(Modifier.height(8.dp))
+            Text("Tu bebé come papilla X veces al día",
+                fontFamily = SpaceMono,
+                fontWeight = FontWeight.Normal,
+                fontSize = 13.sp,
+                textAlign = TextAlign.Center
+            )
+            Spacer(Modifier.height(8.dp))
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
