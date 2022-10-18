f0f78254a13121160edd9bdce8a6e92940cc5a9a
Devany15 <76234922+Devany15@users.noreply.github.com>
2022-10-18 15:18:26 -0500

    INICIO DE MENU SIDEBAR

--- app/src/main/java/com/example/ldmnannyapp/Alimentos.kt
+++ app/src/main/java/com/example/ldmnannyapp/Alimentos.kt
@@ -8,8 +8,11 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ComposeCompilerApi
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -22,6 +25,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import com.example.ldmnannyapp.ui.theme.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class Alimentos : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,32 +47,72 @@ class Alimentos : ComponentActivity() {
 
 @Composable
 fun vistaAlimentos() {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        topBarAlimentos()
-        Spacer(Modifier.height(15.dp))
-        titleAlimentos()
-        Spacer(Modifier.height(15.dp))
-        regAlimentos()
-        Spacer(Modifier.height(30.dp))
-        estAlimentos()
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    Scaffold (
+        scaffoldState = scaffoldState,
+        topBar = { topBarAlimentos(scope, scaffoldState)},
+        drawerContent = {DrawerAlimentos()}
+    )
+    {
+        Column(Modifier.width(600.dp)) {
+            Spacer(Modifier.height(25.dp))
+            titleAlimentos()
+            Spacer(Modifier.height(15.dp))
+            regAlimentos()
+            Spacer(Modifier.height(30.dp))
+            estAlimentos()
+        }
     }
-
 }
 
 @Composable
-fun topBarAlimentos()
-{
-    TopAppBar()
-    {
-        Text("Mariana",
-            textAlign = TextAlign.Center,
-            color = letra,
-            fontWeight = FontWeight.Bold,
-            fontFamily = MeriendaOne,
-            fontSize = 30.sp,
-            modifier = Modifier
-                .fillMaxWidth()
-        )
+fun topBarAlimentos(
+    scope: CoroutineScope,
+    scaffoldState: ScaffoldState
+) {
+    TopAppBar(
+        title = {
+            Text(
+                "Mariana",
+                textAlign = TextAlign.Center,
+                color = letra,
+                fontWeight = FontWeight.Bold,
+                fontFamily = MeriendaOne,
+                fontSize = 30.sp,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = {
+                scope.launch {
+                    scaffoldState.drawerState.open()
+                }
+            }) {
+                Icon(imageVector = Icons.Filled.Menu,
+                contentDescription = "Icono de menú")
+            }
+        }
+    )
+}
+
+@Composable
+fun DrawerAlimentos(){
+    val menu_items = listOf(
+        "Alimentos",
+        "Cambio de pañal",
+        "Siestas",
+        "Otros"
+    )
+    Column(){
+        menu_items.forEach{item ->
+            TextButton(onClick = { /*TODO*/ }) {
+                Text(item,
+                modifier = Modifier
+                    .fillMaxWidth())
+            }
+        }
     }
 }
 
