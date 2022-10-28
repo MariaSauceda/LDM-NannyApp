8e2ccc7b19b4ca38b3c8a4851623da8a6707ba56
RogerGOrtiz <115331147+RogerGOrtiz@users.noreply.github.com>
2022-10-27 21:32:10 -0500

    Nada

--- app/src/main/java/com/example/ldmnannyapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/ldmnannyapp/ui/theme/Color.kt
@@ -10,3 +10,4 @@ val Teal200 = Color(0xFF03DAC5)
 val fondo = Color(0xFFF6D2AE)//CARDS Y FONDO
 val letra = Color(0xFFEA5A47)
 val amarillito = Color(0xFFFFECD9)
+val rojito = Color(0xFFD22F27)//Color del boton y radiobutton
